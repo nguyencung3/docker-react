87fd8b6ce6303cf00220451f35f0e59d7af13312
Cung Nguyen <cungnguyen@Cungs-MacBook-Pro.local>
2021-12-21 12:07:48 +0700

    added travis file

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import { getByText, render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = getByText("learn react");
+//   expect(linkElement).toBeInTheDocument();
+// });
+test("everything is a node", () => {
+  const Foo = () => <div>Hello</div>;
+  render(<Foo />);
+  expect(screen.getByText("Hello")).toBeInstanceOf(Node);
 });
